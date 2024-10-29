7f8a3e65fd97c138acb0019dae3c476a34261f4b
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-29 15:42:05 -0300

    feat: create ccomponents folder

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,12 @@
 import "./App.css";
+import { Form } from "./components/form";
 
 function App() {
-  return <></>;
+  return (
+    <main className="">
+      <Form />
+    </main>
+  );
 }
 
 export default App;
---
+++ src/components/form.tsx
@@ -0,0 +1,7 @@
+export function Form() {
+  return (
+    <form>
+      <h2>Formulário Dinâmico</h2>
+    </form>
+  );
+}
