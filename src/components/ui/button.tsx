a0e664ad098bfde24eb60d2f365b355cc1d6231f
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-29 20:07:47 -0300

    style: add layout to button

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -3,7 +3,12 @@ import { ButtonProps } from '../../utils/types';
 
 export function Button({ children, className, ...props }: ButtonProps) {
   return (
-    <button className={cn('', className)} {...props}>
+    <button
+      className={cn(
+        'w-full h-8 px-2 bg-slate-300 shadow-sm shadow-black hover:scale-95 hover:shadow-none',
+        className,
+      )}
+      {...props}>
       {children}
     </button>
   );
