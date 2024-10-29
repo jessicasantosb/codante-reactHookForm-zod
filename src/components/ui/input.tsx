3d29b0e747a4286e23a42a749699b972ae523df4
Jessica Bandeira <jessica.santosb@outlook.com>
2024-10-29 20:08:16 -0300

    style: add layout to input

--- src/components/ui/input.tsx
+++ src/components/ui/input.tsx
@@ -2,5 +2,13 @@ import { cn } from '../../lib/utils';
 import { InputProps } from '../../utils/types';
 
 export function Input({ className, ...props }: InputProps) {
-  return <input className={cn('', className)} {...props} />;
+  return (
+    <input
+      className={cn(
+        'h-8 p-2 rounded-sm border border-slate-400 ring-offset-slate-400 placeholder:text-slate-400 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-slate-500 focus-visible:ring-offset-1 disabled:cursor-not-allowed disabled:opacity-50',
+        className,
+      )}
+      {...props}
+    />
+  );
 }
