13ca8e0cd64a4270cb13dd74a0b331070393c004
Jessica Bandeira <jessica.santosb@outlook.com>
2024-11-19 18:49:10 -0300

    feat: create zod schema

---
+++ src/schema.ts
@@ -0,0 +1,43 @@
+import { z } from 'zod';
+
+export const userRegisterSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: 'O campo nome precisa ser preenchido' })
+    .max(255, { message: 'O nome deve ter no máximo 255 caracteres' }),
+  email: z
+    .string()
+    .min(1, { message: 'O campo email precisa ser preenchido' })
+    .email({ message: 'Email inválido' }),
+  password: z
+    .string()
+    .min(8, { message: 'O campo senha deve ter no mínimo 8 caracteres' }),
+  password_confirmation: z.string().min(8, {
+    message: 'O campo confirmação de senha deve ter no mínimo 8 caracteres',
+  }),
+  phone: z
+    .string()
+    .min(1, { message: 'O campo número de celular precisa ser preenchido' })
+    .regex(/^\(\d{2}\) \d{5}-\d{4}$/, {
+      message: 'Número de celular inválido',
+    }),
+  cpf: z
+    .string()
+    .min(1, { message: 'O campo CPF precisa ser preenchido' })
+    .regex(/^\d{3}\.\d{3}\.\d{3}-\d{2}$/, {
+      message: 'Número de celular inválido',
+    }),
+  zipcode: z
+    .string()
+    .min(1, { message: 'O campo CEP precisa ser preenchido' })
+    .regex(/^\d{5}-\d{3}$/, {
+      message: 'CEP inválido',
+    }),
+  address: z
+    .string()
+    .min(1, { message: 'O campo endereço precisa ser preenchido' }),
+  city: z.string().min(1, { message: 'O campo cidade precisa ser preenchido' }),
+  terms: z.boolean({ message: 'Você precisa aceitar os termos de uso' }),
+});
+
+export type UserRegister = z.infer<typeof userRegisterSchema>;
